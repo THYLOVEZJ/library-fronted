7b09625faf65cf997e0943957b8cbb3f5910e6eb
thylovezj <1105864486@qq.com>
2023-03-21 16:32:21 +0800

    login

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,61 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Layout from '../views/Layout'
 
 Vue.use(VueRouter)
 
 const routes = [
     {
+        path: '/login',
+        name: 'Login',
+        component: () => import('@/views/login/Login')
+    },
+    // 主页
+    {
         path: '/',
-        name: 'Home',
-        component: Home
+        name: 'Layout',
+        component: Layout,
+        redirect: '/home',
+        children: [
+            {
+                path: 'home',
+                name: 'Home',
+                component: () => import('@/views/home/Home')
+            },
+            // user
+            {
+                path: 'user',
+                name: 'User',
+                component: () => import('@/views/user/User')
+            },
+            {
+                path: 'addUser',
+                name: 'AddUser',
+                component: () => import('@/views/user/addUser')
+            },
+            {
+                path: 'editUser',
+                name: 'editUser',
+                component: () => import('@/views/user/editUser')
+            },
+            // admin
+            {
+                path: 'admin',
+                name: 'Admin',
+                component: () => import('@/views/admin/Admin')
+            },
+            {
+                path: 'addAdmin',
+                name: 'AddAdmin',
+                component: () => import('@/views/admin/addAdmin')
+            },
+            {
+                path: 'editadmin',
+                name: 'editAdmin',
+                component: () => import('@/views/admin/editAdmin')
+            }
+        ]
     },
-    // user
-    {
-        path: '/user',
-        name: 'User',
-        component: () => import('@/views/user/User')
-    },
-    {
-        path: '/addUser',
-        name: 'AddUser',
-        component: () => import('@/views/user/addUser')
-    },
-    {
-        path: '/editUser',
-        name: 'editUser',
-        component: () => import('@/views/user/editUser')
-    },
-    // admin
-    {
-        path: '/admin',
-        name: 'Admin',
-        component: () => import('@/views/admin/Admin')
-    },
-    {
-        path: '/addAdmin',
-        name: 'AddAdmin',
-        component: () => import('@/views/admin/addAdmin')
-    },
-    {
-        path: '/editadmin',
-        name: 'editAdmin',
-        component: () => import('@/views/admin/editAdmin')
-    }
 ]
 
 const router = new VueRouter({
---
+++ src/views/login/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div style="height: 100vh;overflow: hidden">
+    <div style="width: 500px;height: 400px;border-radius: 10px;background-color: white;margin:150px auto;padding: 50px">
+      <el-form :model="admin" ref="login" :rules="rules">
+        <div style="margin:30px;text-align: center;font-size: x-large;color: dodgerblue">登 录</div>
+        <el-form-item prop="username">
+          <el-input placeholder="请输入账号" prefix-icon="el-icon-user" size="medium"
+                    v-model="admin.username"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input placeholder="请输入密码" prefix-icon="el-icon-key" size="medium" type="password"
+                    v-model="admin.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width: 100%" @click="login">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import request from "@/utils/request";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      admin: {},
+      rules: {
+        username: [
+          {required: true, message: '请输入用户名', trigger: 'blur'},
+        ],
+        password: [
+          {required: true, message: '请输入密码', trigger: 'blur'},
+        ]
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.$refs['login'].validate((valid) => {
+        if (valid) {
+          request.post("/admin/login", this.admin).then(res => {
+            if (res.code === '200') {
+              this.$notify.success("登录成功")
+              this.$router.push("/")
+            } else {
+              this.$notify.error(res.msg)
+            }
+          })
+        } else {
+          this.$notify.warning("请检查表单")
+        }
+      })
+    }
+  }
+}
+</script>
+
